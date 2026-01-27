505269b239d5c19e7235bcef641b9b16e024a576
daande01 <daniel.evert.andersson@gmail.com>
2026-01-27 10:24:24 +0100

    new slider

--- js/slider/js/slider.js
+++ js/slider/js/slider.js
@@ -1,4 +1,12 @@
-$( "#window" ).on( "click", function() {
+
+$("#window").css("overflow","hidden")
+$("#left").show();
+
+
+$( "#window ,#left" ).on( "click", function() {
+ let p = $("#bilder").position();
+ console.log(p.left);
+ 
   $( "#bilder" ).animate({
     left: "-=200"
   }, 2000, function() {
